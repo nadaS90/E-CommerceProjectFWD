f1f1d94146df18af037bbfd6831b09b31e570bf0
nadaS90 <engnada_45@outlook.com>
2022-08-03 22:42:49 +0200

    Register Feature

---
+++ src/test/java/org/example/testRunner/TestRunner.java
@@ -0,0 +1,23 @@
+package testRunner;
+
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        features = "src\\main\\features",
+        glue = "stepDefinition",
+        tags = "@smoke",
+        plugin = {
+                "pretty",
+                "html:target/cucumber.html",
+                "json:target/cucumber.json",
+                "junit:target/cukes.xml",
+                "rerun:target/rerun.txt"
+        }
+)
+
+public class TestRunner {
+}
